bac9dd258d929a87c7c859faebcf483734ea31de
foursking <lyf021408@gmail.com>
2013-07-01 18:28:32 +0800

    build

---
+++ application/controllers/tips.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *
+ **/
+class VimTips
+{
+
+    function __construct()
+    {
+        // code...
+    }
+}
+
+
+
+
+
+?>
