00f3f889e7748b745d5e960b2981629eba6eab9a
foursking <lyf021408@gmail.com>
2013-07-14 20:21:38 +0800

    添加插件预览样式

--- application/views/plugin_index_view.php
+++ application/views/plugin_index_view.php
@@ -8,7 +8,7 @@
       <div class="span3 sidebar">
         <div class="section-menu">
           <ul class="nav nav-list">
-            <li class="nav-header">In This Section</li>
+            <li class="nav-header">Vim Plugin Here</li>
             <li class="active"><a href="###" class="first" style="padding:15px 0px 15px 30px">all<i class="icon-angle-right"></i></a></li>
             <li><a href="###"  style="padding:15px 0px 15px 30px">colorscheme<i class="icon-angle-right"></i></a></li>
             <li><a href="###"  style="padding:15px 0px 15px 30px">ftplugin<i class="icon-angle-right"></i></a></li>
@@ -22,6 +22,42 @@
 
       <!--main content-->
       <div class="span7">
+
+        <style>
+            .media{
+        -webkit-border-radius: 2px;
+        -moz-border-radius: 2px;
+        border-radius: 2px;
+        border: 1px solid #cdcdcd;
+        padding: 10px;
+        background: #ffffff;
+        -webkit-box-shadow: 1px 1px 5px #cdcdcd;
+        -moz-box-shadow: 1px 1px 5px #cdcdcd;
+        box-shadow: 1px 1px 5px #cdcdcd;
+
+}
+
+
+
+
+        </style>
+        <!-- The team -->
+        <div class="block team margin-top-large" id="team">
+          <div class="media" style="padding:1em;">
+          <div class="pull-left"> <img src="<?php echo base_url("public/img/team/jimi.jpg")?>" class="img-polaroid media-object" alt="Jimi" /> </div>
+            <div class="media-body">
+              <h4 class="media-heading">Jimi</h4>
+              <p>here is description</p>
+              <ul class="inline">
+                <li><a href="#"><i class="icon-twitter"></i> Twitter</a></li>
+                <li><a href="#"><i class="icon-facebook"></i> Facebook</a></li>
+                <li><a href="#"><i class="icon-linkedin"></i> LinkedIn</a></li>
+                <li><a href="#"><i class="icon-google-plus"></i> Google+</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
 
       </div>
 
