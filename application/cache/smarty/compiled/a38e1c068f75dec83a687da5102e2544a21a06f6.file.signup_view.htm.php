5be9836d273805228ec89ef6c490d6514c4efaa4
foursking <lyf021408@gmail.com>
2014-02-10 15:46:25 +0800

    update

--- application/cache/smarty/compiled/a38e1c068f75dec83a687da5102e2544a21a06f6.file.signup_view.htm.php
+++ application/cache/smarty/compiled/a38e1c068f75dec83a687da5102e2544a21a06f6.file.signup_view.htm.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2014-02-08 18:09:08
+<?php /* Smarty version Smarty-3.1.14, created on 2014-02-10 11:52:17
          compiled from "application/views/signup_view.htm" */ ?>
 <?php /*%%SmartyHeaderCode:152820728052f493ba5d2729-62440333%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'a38e1c068f75dec83a687da5102e2544a21a06f6' => 
     array (
       0 => 'application/views/signup_view.htm',
-      1 => 1391854146,
+      1 => 1392004335,
       2 => 'file',
     ),
   ),
@@ -30,29 +30,32 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 			<form action="http://dev.openvim.com/u/register" method="post" accept-charset="utf-8"id =  "register-welcome-form" >
 				<div class="control-group">
 					<input type="text" name="login_email" placeholder="Email地址" class="text-34" style="height:30px" required="" />
+					<span class="text-error"></span>
 				</div>
 				<div class="control-group">
 					<input type="password" name="login_password" placeholder="登陆密码" class="text-34" style="height:30px" required="" />
+                <span class="text-error"></span>
 				</div>
 				<div class="control-group">
 					<div class="controls">
 						<input type="text" name="register_username" placeholder="常用名" class="text-34" style="height:30px" required=""  value="<?php echo $_smarty_tpl->tpl_vars['user']->value['name'];?>
 " />
+                <span class="text-error"></span>
 					</div>
 				</div>
 				<div class="form-actions">
 					<span>
 					</span>
-					<button class="btn btn-primary" type="submit">注 册</button>
+					<button id="login-submit" class="btn btn-primary" type="submit">注 册</button>
 				</div>
 			</form>
 			<style>
 #current_avatar {
-padding: 5px;
-background-color: #fff;
-box-shadow: 0 1px 3px rgba(34,25,25,.4);
--moz-box-shadow: 0 1px 3px rgba(34,25,25,.4);
--webkit-box-shadow: 0 1px 3px rgba(34,25,25,.4);
+	padding: 5px;
+	background-color: #fff;
+	box-shadow: 0 1px 3px rgba(34,25,25,.4);
+	-moz-box-shadow: 0 1px 3px rgba(34,25,25,.4);
+	-webkit-box-shadow: 0 1px 3px rgba(34,25,25,.4);
 }
 			</style>
 			<div class="form-extra">
--- application/cache/smarty/compiled/adc7053d4a252b17de10fa1ab9cc7416408d5642.file.login_view.htm.php
+++ application/cache/smarty/compiled/adc7053d4a252b17de10fa1ab9cc7416408d5642.file.login_view.htm.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2014-01-28 17:46:37
+<?php /* Smarty version Smarty-3.1.14, created on 2014-02-10 11:49:37
          compiled from "application/views/login_view.htm" */ ?>
 <?php /*%%SmartyHeaderCode:5335152052e77c7d657531-21424311%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'adc7053d4a252b17de10fa1ab9cc7416408d5642' => 
     array (
       0 => 'application/views/login_view.htm',
-      1 => 1390203462,
+      1 => 1391854127,
       2 => 'file',
     ),
   ),
@@ -15,9 +15,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.14',
   'unifunc' => 'content_52e77c7d6a0288_49582471',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_52e77c7d6a0288_49582471')) {function content_52e77c7d6a0288_49582471($_smarty_tpl) {?>
 <div id="content">
@@ -30,7 +30,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                 <span class="text-error"></span>
             </div>
             <div class="control-group" style="position:relative">
-                <input type="password" name="login_password" placeholder="登陆密码" class="text-34" style="height:30px" required="" data-validation-matches-match="pass" data-validation-matches-message="Must match password entered above" />
+                <input type="password" name="login_password" placeholder="登陆密码" class="text-34" style="height:30px" required="" />
                 <span class="text-error"></span>
             </div>
             <div class="form-actions">
